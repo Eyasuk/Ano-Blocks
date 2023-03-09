ce4f2cf47c917405f99e3da0ac70d2a71a3f6cc9
Eyasuk <eyasukibru290@gmail.com>
2023-03-09 17:41:25 +0300

    import wallet field finshed

--- apps/web/components/modules/createwallet_card/importwallet/importpassphrase./index.tsx
+++ apps/web/components/modules/createwallet_card/importwallet/importpassphrase./index.tsx
@@ -13,11 +13,31 @@ export default function ImportPassphrase({
   setpassPhrase,
   stateChanger,
 }: PassphraseTypes): JSX.Element {
-  //const [passphrase, setPassphrase] = useState<string[]>([]);
+  const [copy, setCopie] = useState<boolean>(false);
   const passphraseFetchedRef = useRef<boolean | null>(false);
   //const [userPassphrase, setPassphrase] = useState<Array<string>>(
   //);
-  //useEffect(() => {}, []);
+  useEffect(() => {
+    for (let i = 0; i < 12; i++) {
+      const c = document.getElementById('input' + i) as HTMLInputElement;
+      if (c.value != '') {
+        const passphraseArray = c.value.split(' ');
+        if (passphraseArray.length == 1) {
+          return;
+        }
+        if (passphraseArray.length != 12) {
+          notification({
+            message: 'Incorrect passphrase',
+            messageType: 'error',
+            description:
+              'Make sure to to copy all passphrases separted by only space',
+          });
+          return;
+        }
+        setpassPhrase(passphraseArray);
+      }
+    }
+  }, [copy]);
 
   const handleSumbit = async (event: any) => {
     event.preventDefault();
@@ -41,26 +61,11 @@ export default function ImportPassphrase({
   };
 
   const handelCopy = () => {
-    for (let i = 0; i < 12; i++) {
-      const c = document.getElementById('input' + i) as HTMLInputElement;
-      if (c.value != '') {
-        const passphraseArray = c.value.split(' ');
-        if (passphraseArray.length != 12) {
-          notification({
-            message: 'Incorrect passphrase',
-            messageType: 'error',
-            description:
-              'Make sure to to copy all passphrases separted by only space',
-          });
-          return;
-        }
-        setpassPhrase(passphraseArray);
-        return;
-      }
-    }
+    setCopie(!copy);
   };
 
   const onChange = (value: string, index: number) => {
+    console.log(value);
     let temp = [];
     for (let i = 0; i < 12; i++) {
       if (i === index) {
@@ -79,16 +84,14 @@ export default function ImportPassphrase({
         <p className={styles.description}>
           {' '}
           Please Enter Your PassPhrase Correctly by Entering One by One or Copy
-          it in One Field!!
+          it in The First Field!!
         </p>
 
         <div className={styles.passphrase}>
           {passPhrase.map((content, index) => {
             return (
               <div className={styles.input} key={index}>
-                <p className={styles.index}>
-                  {index + 1 + '.' + passPhrase[0]}
-                </p>
+                <p className={styles.index}>{index + 1 + '.'}</p>
                 <div className={styles.wordinputfield} key={index}>
                   <Input
                     value={passPhrase[index]}
@@ -98,9 +101,7 @@ export default function ImportPassphrase({
                     name={'input' + index}
                     id={'input' + index}
                     onPasteCapture={handelCopy}
-                    onChange={(event) =>
-                      onChange(event.target.innerText, index)
-                    }
+                    onChange={(event) => onChange(event.target.value, index)}
                   />
                 </div>
               </div>
