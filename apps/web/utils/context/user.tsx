4fdb1e59949e8e3e5693389922c74670f41e4214
Eyasuk <eyasukibru290@gmail.com>
2023-03-25 11:31:51 +0300

    refactor

--- apps/web/utils/context/user.tsx
+++ apps/web/utils/context/user.tsx
@@ -44,58 +44,32 @@ export const UserProvider = ({ children }: UserProviderProps): JSX.Element => {
 
   const redirectTo = async (path: string): Promise<void> => {
     const user = checkIfUserLogin();
-    console.log('log');
 
     if (!user) {
-      console.log(1);
-
       if (Object.values(Routes.authenticationRoutes).includes(path)) {
-        console.log(2);
         return;
-        // router.push(path);
       } else {
-        console.log(4);
-
         router.push(Routes.authenticationRoutes.intro);
-        console.log(5);
       }
-      console.log(6);
 
       return;
     }
-    console.log(7);
     if (Object.values(Routes.authorizationRoutes).includes(path)) {
       return;
     }
     if (Object.values(Routes.authenticationRoutes).includes(path)) {
-      console.log(path);
-      console.log(Object.values(Routes.authorizationRoutes));
-      console.log(Object.values(Routes.authorizationRoutes).includes(path));
-      console.log(8);
-
       router.push(Routes.authorizedRoutes.home);
-      console.log(9);
       return;
     }
-    console.log(10);
     if (userLoggedin) {
-      console.log(11);
-
       return;
     }
-    console.log(12);
 
     const userSession = await checkUserSession();
-    console.log(13);
 
     if (userSession) {
-      console.log(14);
-
       if (typeof userSession.password == 'string') {
-        console.log(15);
-
         const auth = await checkUserWithPassword(userSession.password);
-        console.log(16);
 
         if (
           auth &&
@@ -104,39 +78,24 @@ export const UserProvider = ({ children }: UserProviderProps): JSX.Element => {
           typeof auth.pubad == 'string' &&
           typeof auth.pubkey == 'string'
         ) {
-          console.log(17);
-
           const user: UserLoginInfo = {
             hdPriv: auth.hdPriv,
             priv: auth.priv,
             pubad: auth.pubad,
             pubkey: auth.pubkey,
           };
-          console.log(18);
 
           setUserInfo(user);
-          console.log(19);
-
           setUserLoggedin(true);
-          console.log(20);
         }
-        console.log(21);
       }
       return;
     }
-    console.log(22);
 
     const params = new URLSearchParams(searchParams);
-    console.log(23);
-
-    //params.set('redirect', path);
-    console.log(24);
-
+    params.set('redirect', path);
     const redirect = params;
-    console.log(25);
-
     router.push(`${Routes.authorizationRoutes.auth}?${redirect}`);
-    console.log(26);
   };
 
   useEffect(() => {
