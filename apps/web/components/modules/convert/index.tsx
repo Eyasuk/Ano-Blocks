0f02e5339a2f49f8950157d484ed93e57f363203
Eyasuk <eyasukibru290@gmail.com>
2023-05-16 22:51:03 +0300

    home page almost done

--- apps/web/components/modules/convert/index.tsx
+++ apps/web/components/modules/convert/index.tsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 import { Divider, Form, Input, Select, Typography } from "antd";
 import { GlassCard } from "components/elements/cards";
 import { Curriences } from "utils/constants/currencies";
+import { useUserBalance } from "utils/context/userBalance";
+import { numberPrecision } from "utils/helpers/numberPrecision";
 
 import styles from "./convert.module.scss";
 
 const { Text, Title } = Typography;
 
 export default function Convert({ asset }: any) {
-  const [selectAsset, setSelectAsset] = useState<boolean>(false);
+  const { userBalance } = useUserBalance();
   const [convertAsset, setConvertAsset] = useState<string>(Curriences[0].name);
+  const [input, setInput] = useState<number>(1);
 
   const onChangeAsset = (_v: any, option: any) => {
     setConvertAsset(option.value);
-    setSelectAsset(false);
+  };
+
+  const onChangeInput = (event: any) => {
+    let timer;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      console.log(event.target.value);
+      setInput(event.target.value);
+    }, 1000);
   };
 
   return (
@@ -32,7 +43,8 @@ export default function Convert({ asset }: any) {
                     <Divider type="vertical" />
                   </>
                 }
-                min={0}
+                min={0.001}
+                onChange={onChangeInput}
               />
             </Form.Item>
             <Form.Item name="to">
@@ -57,6 +69,15 @@ export default function Convert({ asset }: any) {
               />
             </Form.Item>
           </Form>
+          <Title level={4}>{`${input} ${asset.name} = ${
+            convertAsset == "ETB"
+              ? numberPrecision(
+                  input * 55 * (userBalance as any)[asset.name]["price"]
+                )
+              : numberPrecision(
+                  input * (userBalance as any)[asset.name]["price"]
+                )
+          } ${convertAsset}`}</Title>
         </div>
       )}
     </GlassCard>
