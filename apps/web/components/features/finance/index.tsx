69eb921f08fe87c66b12261dd9ded575702a67c8
Eyasuk <eyasukibru290@gmail.com>
2023-06-01 21:29:18 +0300

    start finance page

---
+++ apps/web/components/features/finance/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Col, Row } from "antd";
+import Default from "components/layouts/default";
+
+export default function Finance(): JSX.Element {
+  return (
+    <Default>
+      <Row
+        gutter={[
+          { xs: 15, sm: 4, md: 4, lg: 26 },
+          { xs: 15, sm: 4, md: 4, lg: 20 },
+        ]}
+      >
+        <Col md={10} xs={24} span={12}>
+          <div></div>{" "}
+        </Col>
+        <Col md={12} xs={24} span={22}>
+          <div></div>{" "}
+        </Col>
+        <Col md={12} xs={24} lg={12} span={6}>
+          <div></div>{" "}
+        </Col>
+        <Col md={12} xs={24} lg={10} span={6}>
+          <div></div>{" "}
+        </Col>
+      </Row>
+    </Default>
+  );
+}
