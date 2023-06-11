fa7ce2a67164d4233c3ebede97b3ca1e2f529e05
Eyasuk <eyasukibru290@gmail.com>
2023-06-12 00:02:59 +0300

    send history

--- apps/web/components/features/homepage/index.tsx
+++ apps/web/components/features/homepage/index.tsx
@@ -15,18 +15,18 @@ export default function HomePage(): JSX.Element {
           { xs: 15, sm: 4, md: 4, lg: 20 },
         ]}
       >
-        <Col md={10} xs={24} span={12}>
+        <Col md={24} sm={24} xs={24} lg={9} span={12}>
           <Wallet />
         </Col>
-        <Col md={12} xs={24} span={22}>
+        <Col md={24} sm={24} xs={24} lg={10} span={12}>
           <EtbcWallet />
         </Col>
-        <Col md={12} xs={24} lg={12} span={6}>
+        <Col md={24} sm={24} xs={24} lg={14}>
           <Assets />
         </Col>
-        <Col md={12} xs={24} lg={10} span={6}>
+        {/* <Col md={12} xs={24} lg={10} span={6}>
           <Postion />
-        </Col>
+        </Col> */}
       </Row>
     </Default>
   );
